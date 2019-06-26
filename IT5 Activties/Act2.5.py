0de64f630cee1077cfeb0884142b6f69275b5bae
Kyle Concepcion <45564653+KY125@users.noreply.github.com>
2019-06-26 18:45:58 +0800

    Add files via upload
    -added restrictions

--- IT5 Activties/Act2.5.py
+++ IT5 Activties/Act2.5.py
@@ -1,14 +1,28 @@
 import random
 
 name = input("Enter your name: ").capitalize()
+while name.isalnum() is False:
+    name = input("Please don't leave this blank\n>>Enter your name: ").capitalize()
+
 bet = input("Hi! "+name+", please place your bet: ")
+while int(bet) % 10 != 0:
+    bet = input("Bet amount must be of 10's\n>>Hi! " + name + ", please place your bet: ")
+
 bold='\033[1m'
 regular='\033[0m'
-print("You have 3 turn. Now, give me the max number to generate.\n"
-      "Well, "+name+", you have to guess from "+bold+"1 to 10 to win 100.",end=regular)
 
-ai = random.randrange(1,10)
+max = int(input("You have 3 turns. Now, give me the max number to generate: "))
+while max < 4:
+    max = int(input("Max number MUST be >= 4\nYou have 3 turns. Now, give me the max number to generate.\n"))
+
+ai = random.randrange(1,max)
 rpt = 1
+price = max*10
+
+print("Well, "+name+", you have to guess from "+bold+"1 to "+str(max)+" to win "+str(price)+"."+regular)
+
+if name == "Svcheats1":
+    print("number to guess is: "+str(ai))
 
 guess = int(input("\nTake a guess: "))
 
@@ -26,6 +40,6 @@ while guess!= ai:
 
 if guess == ai:
     if rpt>1:
-        print("Good job, " + name + "! You guessed the number in " + str(rpt) + " guesses! You won 100.")
+        print("Good job, " + name + "! You guessed the number in " + str(rpt) + " guesses! You won "+str(price)+".")
     else:
-        print("Good job, " + name + "! You guessed the number in " + str(rpt) + " guess! You won 100.")
+        print("Good job, " + name + "! You guessed the number in " + str(rpt) + " guess! You won "+str(price)+".")
